a06b604fcf51ff28ae60c2baa86cfd987b019984
Azure-stars <Azure_stars@126.com>
2025-05-08 14:29:06 +0000

    Deploying to gh-pages from @ oscomp/starry-next@7014eee55d3da473fa2e5e194d9157bf7310cb76 🚀

---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["starry_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;VfsNodeType&gt; for <a class=\"enum\" href=\"starry_api/enum.FileType.html\" title=\"enum starry_api::FileType\">FileType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;stat&gt; for <a class=\"struct\" href=\"starry_api/struct.Kstat.html\" title=\"struct starry_api::Kstat\">Kstat</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[554]}
